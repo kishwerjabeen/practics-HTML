bc53fd2bca31aa5ea81e618d6e72485d8177e993
Kishwar Jabeen <43556329+kishwerjabeen@users.noreply.github.com>
2019-01-10 09:39:35 -0800

    Add files via upload

---
+++ Js Practise/2018-01-03/test.js
@@ -0,0 +1,4 @@
+function abc(){
+document.getElementById("demo").innerHTML= (6-3+2*4/2);
+// 10	-1	1	-3		re   7		err
+	}
---
+++ Js Practise/test.js
@@ -0,0 +1,18 @@
+function Test1(){
+	var a = 5;
+	var b = 10;
+	var c = a+b;
+document.getElementById("v1").innerHTML=a + "+" + b + "=" + c;
+	}
+function Test2(){
+	var a = 50;
+	var b = 10;
+	var c = a-b;
+document.getElementById("v2").innerHTML=a + "-" + b + "=" + c; 
+	}
+function Test3(){
+	var a = 13;
+	var b = 3;
+	var c = a%b;
+document.getElementById("v3").innerHTML=a + "%" + b + "=" + c; 
+	}	
